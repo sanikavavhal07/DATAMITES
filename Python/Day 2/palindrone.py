1cda1306bdf9fa6e950a3999304fdb66673c629e
Sanika Vavhal <sanikavavhal007@gmail.com>
2024-07-24 22:46:03 +0530

    Update palindrome.py

--- Python/Day 2/palindrone.py
+++ Python/Day 2/palindrone.py
@@ -1,6 +1,13 @@
-palindrome = int(input("Enter the Number : "))
-num = 1
-while(num<=palindrome):
-    rem = palindrome % 10
-    num += 1
-    print(rem)
+num = int(input("Enter the number: "))
+original_num = num
+reversed_num = 0
+
+while num > 0:
+    rem = num % 10
+    reversed_num = reversed_num * 10 + rem
+    num = num // 10
+
+if original_num == reversed_num:
+    print(f"{original_num} is a palindrome.")
+else:
+    print(f"{original_num} is not a palindrome.")
